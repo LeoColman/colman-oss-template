b271e806d38fa3324eb1ae3201c93e6b1b5b2de8
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2023-04-30 20:04:16 -0400

    Upgrades the template

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # FIXME TITLE
 
 FIXME badges
-
-![Build](https://github.com/LeoColman/)
-[![GitHub](https://img.shields.io/github/license/LeoColman/)](https://github.com/LeoColman/SimpleCpfValidator/blob/master/LICENSE) [![Maven Central](https://img.shields.io/maven-central/v/group/simple-cpf-validator.svg)](https://search.maven.org/search?q=g:group)
+![Maintenance](https://img.shields.io/maintenance/yes/2023)
+![Build](https://github.com/LeoColman/)  // FIXME
+[![License](https://img.shields.io/github/license/LeoColman/kotest)](LICENSE) // FIXME
+![Maven Central](https://img.shields.io/maven-central/v/br.com.colman/kotest-runner-android) // FIXME
 
 
 SHORT DESCRIPTION
--- build.gradle.kts
+++ build.gradle.kts
@@ -18,111 +18,106 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.lang.System.getenv
 
 plugins {
-    kotlin("jvm") version "1.7.10"
-    `maven-publish`
-    signing
-    id("org.jetbrains.dokka") version "1.7.10"
-    id("io.gitlab.arturbosch.detekt").version("1.15.0-RC1")
+  kotlin("jvm") version libs.versions.kotlin.get()
+  id("org.jetbrains.dokka") version libs.versions.kotlin.get()
+  alias(libs.plugins.detekt)
+  `maven-publish`
+  signing
 }
 
-// FIXME Replace with group
-group = "br.com.colman."
+group = "br.com.colman"
 version = getenv("RELEASE_VERSION") ?: "local"
 
-repositories {
-    mavenCentral()
-    jcenter()
+dependencies {
+  // Kotest
+  testImplementation(libs.bundles.kotest)
 }
 
-dependencies {
-    // Kotest
-    testImplementation("io.kotest:kotest-runner-junit5:5.4.2")
-    
-    // Mockk
-    testImplementation("io.mockk:mockk:1.12.7")
+detekt {
+  buildUponDefaultConfig = true
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+  kotlinOptions.jvmTarget = "1.8"
 }
 
 tasks.withType<Test> {
-    useJUnitPlatform()
+  useJUnitPlatform()
 }
 
 kotlin {
-    explicitApi()
+  explicitApi()
 }
 
 val sourcesJar by tasks.registering(Jar::class) {
-    archiveClassifier.set("sources")
-    from(sourceSets.getByName("main").allSource)
+  archiveClassifier.set("sources")
+  from(sourceSets.getByName("main").allSource)
 }
 
 val javadocJar by tasks.registering(Jar::class) {
-    dependsOn("dokkaHtml")
-    archiveClassifier.set("javadoc")
-    from("$buildDir/dokka")
+  dependsOn("dokkaHtml")
+  archiveClassifier.set("javadoc")
+  from("$buildDir/dokka")
 }
 
 
 publishing {
-    repositories {
+  repositories {
 
-        maven("https://oss.sonatype.org/service/local/staging/deploy/maven2") {
-            credentials {
-                username = getenv("OSSRH_USERNAME")
-                password = getenv("OSSRH_PASSWORD")
-            }
-        }
+    maven("https://oss.sonatype.org/service/local/staging/deploy/maven2") {
+      credentials {
+        username = getenv("OSSRH_USERNAME")
+        password = getenv("OSSRH_PASSWORD")
+      }
     }
+  }
 
-    publications {
+  publications {
 
-        register("mavenJava", MavenPublication::class) {
-            from(components["java"])
-            artifact(sourcesJar.get())
-            artifact(javadocJar.get())
+    register("mavenJava", MavenPublication::class) {
+      from(components["java"])
+      artifact(sourcesJar.get())
+      artifact(javadocJar.get())
 
-            pom {
-                name.set("NAME")    // FIXME change name
-                description.set("DESCRIPTION") //FIXME change description
-                url.set("https://www.github.com/LeoColman/repo") // FIXME change URL
+      pom {
+        name.set("NAME")    // FIXME change name
+        description.set("DESCRIPTION") //FIXME change description
+        url.set("https://www.github.com/LeoColman/repo") // FIXME change URL
 
 
-                scm {
-                    connection.set("scm:git:http://www.github.com/LeoColman/repo") // FIXME change URL
-                    developerConnection.set("scm:git:http://github.com/LeoColman/repo") // FIXME change URL
-                    url.set("https://www.github.com/LeoColman/repo") // FIXME change URL
-                }
-
-                licenses {
-                    license {
-                        name.set("The Apache 2.0 License")
-                        url.set("https://opensource.org/licenses/Apache-2.0")
-                    }
-                }
-
-                developers {
-                    developer {
-                        id.set("LeoColman")
-                        name.set("Leonardo Colman Lopes")
-                        email.set("leonardo.dev@colman.com.br")
-                    }
-                }
-            }
+        scm {
+          connection.set("scm:git:http://www.github.com/LeoColman/repo") // FIXME change URL
+          developerConnection.set("scm:git:http://github.com/LeoColman/repo") // FIXME change URL
+          url.set("https://www.github.com/LeoColman/repo") // FIXME change URL
         }
+
+        licenses {
+          license {
+            name.set("The Apache 2.0 License")
+            url.set("https://opensource.org/licenses/Apache-2.0")
+          }
+        }
+
+        developers {
+          developer {
+            id.set("LeoColman")
+            name.set("Leonardo Colman Lopes")
+            email.set("dev@leonardo.colman.com.br")
+          }
+        }
+      }
     }
+  }
 }
 
 val signingKey: String? by project
 val signingPassword: String? by project
 
 signing {
-    useGpgCmd()
-    if(signingKey != null && signingPassword != null) {
-        useInMemoryPgpKeys(signingKey, signingPassword)
-    }
+  useGpgCmd()
+  if (signingKey != null && signingPassword != null) {
+    useInMemoryPgpKeys(signingKey, signingPassword)
+  }
 
-    sign(publishing.publications["mavenJava"])
+  sign(publishing.publications["mavenJava"])
 }
